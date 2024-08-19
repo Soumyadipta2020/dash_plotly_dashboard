b6657de12c034813bd4ed1461a422fbbae70bdfe
Soumyadipta2020 <soumyadipta048@gmail.com>
2024-08-19 22:51:55 +0530

    data selected

--- app.py
+++ app.py
@@ -2,3 +2,16 @@ from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
 
+# source - https://www.kaggle.com/datasets/myrios/cost-of-living-index-by-country-by-number-2024?resource=download
+df = pd.read_csv('./Cost_of_Living_Index_by_Country_2024.csv', header = 0)
+
+app = Dash(__name__)
+
+app.layout = [
+    html.H1(children='Cost of Living Index by Country 2024', style={'textAlign':'center'})
+]
+
+
+# Run app
+if __name__ == '__main__':
+    app.run(debug=True)
