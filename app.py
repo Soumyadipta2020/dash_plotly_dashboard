2e2e6097f8afde02f2a9245ff5040315024b09dc
Soumyadipta2020 <soumyadipta048@gmail.com>
2024-08-23 00:17:17 +0530

    bar, violin, line plot added

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
+import dash
 from dash import Dash, html, dcc, callback, Output, Input, dash_table
 import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import pandas as pd
 
 # Read data
 df = pd.read_csv('./Cost_of_Living_Index_by_Country_2024.csv', header = 0)
+
+# column names
+column_names = df.columns.tolist()[2:]
+# Unique values from Country
+unique_country = df['Country'].unique()
 
 app = Dash(__name__)
 
 # Layout
 app.layout = [
     html.H1(children='Cost of Living Index by Country 2024', style={'textAlign':'center'}),
-    html.Content(children='Data Source - '),
-    html.A(children="Link", \
-           href = "https://www.kaggle.com/datasets/myrios/cost-of-living-index-by-country-by-number-2024?resource=download"),
+    html.Content(children='Data Source - ', style = {'fontWeight': 'bold'}),
+    html.A(children="Kaggle", href = "https://www.kaggle.com/datasets/myrios/cost-of-living-index-by-country-by-number-2024?resource=download"),
     html.Br(),
     html.Content(children='The cost of living indices are relative to New York City (NYC), with a baseline index of 100% for NYC. \
                  Here is a breakdown of each index and its meaning:'),
@@ -51,9 +58,55 @@ app.layout = [
             'backgroundColor': 'lightgrey',
             'fontWeight': 'bold'
         }
-    )
+    ), 
+    html.H1(children="Visualize Index", style = {'textalign':'left'}),
+    html.Div([
+        html.Div([
+            html.Label("Index Type",  style = {'fontWeight': 'bold'}),
+            dcc.Dropdown(options=[{'label': col, 'value': col} for col in column_names],
+                     value=column_names[0],
+                     id='col-dropdown', multi=True, clearable=False, 
+                     placeholder = 'Index Type')
+        ], style={'width': '28%', 'display': 'inline-block'}),
+        html.Div([
+            html.Label("Countries",  style = {'fontWeight': 'bold'}),
+            dcc.Dropdown(options=[{'label': val, 'value': val} for val in unique_country],
+                     value=list(unique_country)[0:5],
+                     id='country-dropdown', multi=True, clearable=False, 
+                     placeholder = 'Country')
+        ], style={'width': '48%', 'display': 'inline-block', 'paddingLeft': '10px'}),
+        html.Div([
+            html.Label("Plot Type",  style = {'fontWeight': 'bold'}),
+            dcc.Dropdown(options=['Line Plot', 'Violin Plot', 'Bar Plot'],
+                     value='Line Plot',
+                     id='plot-dropdown', multi=False, clearable=False, 
+                     placeholder = 'Plot Type')
+        ], style={'width': '20%', 'display': 'inline-block', 'paddingLeft': '10px'}),
+        dcc.Graph(id='graph-output')
+    ]),
 ]
 
+# Graph plot
+@app.callback(
+    Output('graph-output', 'figure'),
+    [Input('col-dropdown', 'value'),
+    Input('country-dropdown', 'value'),
+    Input('plot-dropdown', 'value')]
+)
+
+def update_graph(selected_column, selected_entries, selected_plot):
+    if not selected_entries:
+        raise dash.exceptions.PreventUpdate
+    
+    filtered_df = df[df['Country'].isin(selected_entries)]
+
+    if selected_plot == 'Line Plot':
+        fig = px.line(filtered_df, x = 'Country', y = selected_column, title = 'Country vs Index')
+    elif selected_plot == 'Violin Plot':
+        fig = px.violin(filtered_df, y = selected_column, box = True, title = 'Indices')
+    elif selected_plot == 'Bar Plot':
+        fig = px.bar(filtered_df, x = 'Country', y = selected_column, title = 'Country vs Index', barmode='group')
+    return fig
 
 # Run app
 if __name__ == '__main__':
